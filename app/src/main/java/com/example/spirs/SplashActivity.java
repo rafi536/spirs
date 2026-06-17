e09d6dc69c3137545ba269e94c85f9ae9317592c
rafi <rafiaripin0@gmail.com>
2026-06-17 17:10:00 +0700

    menambahkan fitur splash screen

---
+++ app/src/main/java/com/example/spirs/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.spirs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 3000; // 3 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(
+                    SplashActivity.this,
+                    LoginRegisterActivity.class
+            );
+
+            startActivity(intent);
+            finish(); // tidak bisa kembali ke splash
+
+        }, SPLASH_TIME);
+    }
+}
